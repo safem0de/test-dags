d45f8128fe46818527a235a5680baff50d0c0e28
Safem0de <n.wangwanich@gmail.com>
2025-03-07 00:53:13 +0700

    g

--- dags/airquality.py
+++ dags/airquality.py
@@ -19,7 +19,7 @@ with DAG(
     "airquality_api_dag",
     schedule="*/10 * * * *",
     start_date=timezone.datetime(2025, 3, 6),
-    tags=["capstone","f"]
+    tags=["capstone","g"]
 ):
     start = EmptyOperator(task_id="start")
 
