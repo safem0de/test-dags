0f9cffbf85e8ed48987c19974aeadc4a23bcdd0e
Safem0de <n.wangwanich@gmail.com>
2025-03-06 23:22:46 +0700

    first step of airquality data

---
+++ dags/airquality.py
@@ -0,0 +1,33 @@
+import json
+import requests
+from datetime import datetime, timedelta
+
+from airflow.models import Variable
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
+from airflow.utils import timezone
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+
+DAG_FILE_NAME = "/opt/airflow/dags/data.json"
+
+def _get_airquality_data():
+    API_KEY = Variable.get("air_quality_key")
+    print(f"API Key: {API_KEY}")
+
+with DAG(
+    "airquality_api_dag",
+    schedule="*/10 * * * *",
+    start_date=timezone.datetime(2025, 3, 1),
+    tags=["capstone"]
+):
+    start = EmptyOperator(task_id="start")
+
+    get_airquality_data = PythonOperator(
+        task_id="get_airquality_data",
+        python_callable=_get_airquality_data,
+    )
+
+    end = EmptyOperator(task_id="end")
+
+    start >> get_airquality_data >> end
