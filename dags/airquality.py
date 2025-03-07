cf6ba887ffb4a8f76990a7e97f3a5d724ab7cb54
Naweeparb <naweeparbw@borneothai.com>
2025-03-07 10:51:56 +0700

    longdoo

--- dags/airquality.py
+++ dags/airquality.py
@@ -19,7 +19,7 @@ with DAG(
     "airquality_api_dag",
     schedule="*/10 * * * *",
     start_date=timezone.datetime(2025, 3, 6),
-    tags=["capstone","again"]
+    tags=["capstone","longdoo"]
 ):
     start = EmptyOperator(task_id="start")
 
