98a52b5bf8f022548b233ea1ad6a4cf4a14f0555
Naweeparb <naweeparbw@borneothai.com>
2025-03-07 10:23:38 +0700

    zzz

--- dags/airquality.py
+++ dags/airquality.py
@@ -19,7 +19,7 @@ with DAG(
     "airquality_api_dag",
     schedule="*/10 * * * *",
     start_date=timezone.datetime(2025, 3, 6),
-    tags=["capstone","symbolink"]
+    tags=["capstone","zzz"]
 ):
     start = EmptyOperator(task_id="start")
 
